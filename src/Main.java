5a2341c35448e2a325bc40050538af1d90b81cbc
PabloDiazTumba <pablodiaz147@gmail.com>
2023-09-25 04:20:59 +0200

    First commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int day = 4;
+        switch (day) {
+            case 1:
+                System.out.println("Måndag");
+                break;
+            case 2:
+                System.out.println("Tisdag");
+                break;
+            case 3:
+                System.out.println("Onsdag");
+                break;
+            case 4:
+                System.out.println("Torsdag");
+                break;
+            case 5:
+                System.out.println("Fredag");
+                break;
+            case 6:
+                System.out.println("Lördag");
+                break;
+            case 7:
+                System.out.println("Söndag");
+                break;
+        }
+    }
+}
